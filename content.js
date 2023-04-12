02de4dd0a11a11324f4aad4476657aed6643fe74
hallfay0 <119840103+hallfay0@users.noreply.github.com>
2023-04-12 20:46:42 +0800

    Update content.js

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 function getSelectedInput() {
-  const inputElements = document.querySelectorAll('input, textarea');
+  const inputElements = document.querySelectorAll("input, textarea");
   for (const input of inputElements) {
     if (input === document.activeElement) {
       return input;
@@ -10,42 +10,55 @@ function getSelectedInput() {
 
 function translateText(text) {
   return new Promise((resolve, reject) => {
-    const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${encodeURIComponent(text)}`;
-fetch(url)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then((data) => {
-    if (data && data[0] && data[0][0] && data[0][0][0]) {
-      resolve(data[0][0][0]);
-    } else {
-      reject(new Error('Translation failed'));
-    }
-  })
-  .catch((error) => {
-    reject(error);
-  });});
+    const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${encodeURIComponent(
+      text
+    )}`;
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data && data[0] && data[0][0] && data[0][0][0]) {
+          resolve(data[0][0][0]);
+        } else {
+          reject(new Error("Translation failed"));
+        }
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 }
 
 function translateAndReplace() {
-const input = getSelectedInput();
-if (input) {
-const originalText = input.value;
-translateText(originalText)
-.then((translatedText) => {
-input.value = translatedText;
-})
-.catch((error) => {
-console.error('Error during translation:', error);
-});
+  const input = getSelectedInput();
+  if (input) {
+    const originalText = input.value;
+    translateText(originalText)
+      .then((translatedText) => {
+        input.value = translatedText;
+      })
+      .catch((error) => {
+        console.error("Error during translation:", error);
+      });
+  }
 }
+function handleKeydown(event) {
+  const isMac = /Mac|iPod|iPhone|iPad/.test(navigator.platform);
+  const cmdOrCtrl = isMac ? event.metaKey : event.ctrlKey;
+
+  if (cmdOrCtrl && event.shiftKey && event.code === "KeyT") {
+    event.preventDefault();
+    translateAndReplace();
+  }
 }
 
+document.addEventListener("keydown", handleKeydown);
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-if (request.action === 'translateAndReplace') {
-translateAndReplace();
-}
+  if (request.action === "translateAndReplace") {
+    translateAndReplace();
+  }
 });
