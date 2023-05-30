0b6090fbb38b87d75d46281d5b2c24e2c83de1ce
hallfay0 <hallfaytiandi@gmail.com>
2023-05-30 20:02:38 +0800

    t4
    t4

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
-chrome.browserAction.onClicked.addListener((tab) => {
+const browserAction = chrome.browserAction;
+browserAction.onClicked.addListener((tab) => {
   chrome.tabs.sendMessage(tab.id, { action: "translateAndReplace" });
 });
